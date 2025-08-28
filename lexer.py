301c410f1883080b43ac7e8b02530df7b7035280
Fran <fgarcete@alumno.unlam.edu.ar>
2025-08-28 00:33:57 -0300

    first commit. Lexer practically done. Parser in progress.

--- lexer.py
+++ lexer.py
@@ -1,45 +1,118 @@
-import ply.lex as lex
+from ply.lex import *
 from pathlib import Path
 import re
 
+# Palabras reservadas
 reserved = {
+    'if': "IF",
+    'else': "ELSE",
+    'while': "WHILE",
+    'init': "INIT",
+    'AND': "AND",
+    'OR': "OR",
+    'NOT': "NOT",
+    'read': 'READ',
+    'write': 'WRITE',
+    'Float': 'TIPO_FLOAT',
+    'Int': 'TIPO_INT',
+    'String': 'TIPO_STRING'
 }
 
+# El orden de los elementos de la lista de TOKENS no importa.
 tokens = [
     'A_PARENTESIS',
     'C_PARENTESIS',
+
+    'A_LLAVES',
+    'C_LLAVES',
+
+    'A_CORCHETES',
+    'C_CORCHETES',
+
+    'COMA',
+    'COLON',
+
     'ASIGNACION',
+
+    'N_FLOAT',
     'N_ENTERO',
+    'STRING',
+
     'VARIABLE',
+
+    'IGUAL',
+    'DISTINTO',
+    'MENOR_IGUAL',
+    'MAYOR_IGUAL',
+    'MENOR',
+    'MAYOR',
+
+    'MAS',
+    'MULTIPLICACION',
     'MENOS',
     'DIVISION',
-    'MULTIPLICACION',
 ] + list(reserved.values())
 
 
 # Expresiones regulares para TOKENS simples
+# El orden de estas definiciones de TOKENS es importante
+
+
+t_MAS =  r'\+'
 t_MENOS = r'-'
 t_MULTIPLICACION = r'\*'
 t_DIVISION = r'/'
-t_A_PARENTESIS = r'\('
-t_C_PARENTESIS = r'\)'
+
+t_IGUAL = r'=='
+t_DISTINTO = r'!='
+t_MENOR_IGUAL = r'<='
+t_MAYOR_IGUAL = r'>='
+t_MENOR = r'<'
+t_MAYOR = r'>'
+
 t_ASIGNACION = r':='
 
+t_A_PARENTESIS = r'\('
+t_C_PARENTESIS = r'\)'
 
-def t_VARIABLE(t):
+t_A_LLAVES = r'\{'
+t_C_LLAVES = r'\}'
+
+t_A_CORCHETES = r'\['
+t_C_CORCHETES = r'\]'
+
+t_COMA = r','
+t_COLON = r':'
+
+
+def t_COMENTARIO(t:LexToken):
+    r'#\+.*?\+#'
+    t.lexer.lineno += t.value.count('\n')
+
+
+def t_VARIABLE(t:LexToken):
     r'[a-zA-Z](\w|_)*'
     t.type = reserved.get(t.value, 'VARIABLE')
     return t
 
+def t_N_FLOAT(t:LexToken):
+    r'(\d+\.\d*|\.\d+)'
+    t.value = float(t.value)
+    return t
 
-def t_N_ENTERO(t):
+def t_N_ENTERO(t:LexToken):
     r'\d+'
     t.value = int(t.value)
     return t
 
+def t_STRING(t:LexToken):
+    r'\"[^\"]*\"'
+    # We remove the quotes from the value so the parser gets the clean string.
+    t.value = t.value[1:-1]
+    return t
 
 # Regla que cuenta la cantidad de lineas
-def t_newline(t):
+def t_newline(t:LexToken):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
@@ -48,16 +121,16 @@ def t_newline(t):
 t_ignore = ' \t'
 
 # Manejo de errores
-def t_error(t):
+def t_error(t:LexError):
     raise Exception(f"Caracter invalido '{t.value[0]}' en la linea: {t.lexer.lineno}")
 
 
 # Build the lexer
-lexer = lex.lex(reflags=re.DOTALL)
+lexer = lex(reflags=re.DOTALL)
 
 
 def ejecutar_lexer():
-    path_lexter = Path('./resources/lexer_test.txt')
+    path_lexter = Path('./resources/lexer_test_2.txt')
     data = path_lexter.read_text()
     lexer.input(data)
     while True:
--- parser.py
+++ parser.py
@@ -2,7 +2,8 @@
 
 # Se importan los tokens generado previamente en el lexer
 from lexer import tokens
-import ply.yacc as yacc  # analizador sintactico
+# import ply.yacc as yacc  
+from ply.yacc import * # analizador sintactico
 from pathlib import Path
 
 diccionarioComparadores = {
@@ -26,18 +27,19 @@ diccionarioComparadoresNot = {
 
 precedence = (
     ('right', 'ASIGNACION'),
-    ('right', 'MENOS'),
+    ('left', 'MENOS'),
+    # ('left', 'MAS'),
     ('left', 'MULTIPLICACION', 'DIVISION'),
     ('left', 'A_PARENTESIS', 'C_PARENTESIS'),
 )
 
 
-def p_start(p):
+def p_start(p:YaccProduction):
     '''start : programa'''
     print('FIN')
 
 
-def p_programa(p):
+def p_programa(p:YaccProduction):
     '''programa : programa sentencia
                 | sentencia
     '''
@@ -47,50 +49,72 @@ def p_programa(p):
         print(f'sentencia -> programa')
 
 
-def p_sentencia(p):
-    '''sentencia : asignacion
+def p_sentencia(p:YaccProduction):
+    '''sentencia : declaracion
+                |  asignacion
     '''
-    print(f'{p.slice[1].type} -> sentencia')
+    print(f'{p.slice[1].type} -> sentencia2 ')
 
+def p_declaracion(p:YaccProduction):
+    '''declaracion : INIT A_LLAVES decl_lista C_LLAVES
+    '''
+    print(f'DECLARACION -> INIT A_LLAVES decl_lista C_LLAVES')
 
-def p_asignacion(p):
+# INT this must be changed to type
+def p_decl_lista(p:YaccProduction):
+    '''decl_lista : var_lista COLON TIPO 
+    '''
+    print(f'decl_lista -> var_lista COLON TIPO')
+
+def p_tipo(p:YaccProduction):
+    '''TIPO : TIPO_INT
+            | TIPO_FLOAT
+            | TIPO_STRING
+    '''
+    print(f'TIPO -> {p.slice[1].type}')
+    # p[0] = p[1]
+
+def p_var_lista(p:YaccProduction):
+    '''var_lista : VARIABLE
+    '''
+    print(f'var_lista -> VARIABLE')
+
+def p_asignacion(p:YaccProduction):
     '''asignacion : VARIABLE ASIGNACION expresion
     '''
     print(f'VARIABLE ASIGNACION {p.slice[3].type} -> asignacion')
 
-
-
-def p_expresion_menos(p):
+def p_expresion_menos(p:YaccProduction):
     'expresion : expresion MENOS termino'
     print('expresion - termino -> expresion')
 
 
-def p_expresion_termino(p):
+def p_expresion_termino(p:YaccProduction):
     'expresion : termino'
     print('termino -> expresion')
 
 
-def p_termino_multiplicacion(p):
+def p_termino_multiplicacion(p:YaccProduction):
     'termino : termino MULTIPLICACION elemento'
     print('termino * elemento -> termino')
 
 
-def p_termino_division(p):
+def p_termino_division(p:YaccProduction):
     'termino : termino DIVISION elemento'
     print('termino / elemento -> termino')
 
 
-def p_termino_elemento(p):
+def p_termino_elemento(p:YaccProduction):
     'termino : elemento'
     print('elemento -> termino')
 
 
-def p_elemento_expresion(p):
+def p_elemento_expresion(p:YaccProduction):
     'elemento : A_PARENTESIS expresion C_PARENTESIS'
     print('( expresion ) -> elemento')
 
 
-def p_elemento(p):
+def p_elemento(p:YaccProduction):
     '''elemento : N_ENTERO
                 | VARIABLE
     '''
@@ -99,13 +123,13 @@ def p_elemento(p):
 
 
 # Error rule for syntax errors
-def p_error(p):
+def p_error(p:YaccError):
     raise Exception(f"Error en la linea {p.lineno or ''} at {p.value or ''}")
 
 
 def ejecutar_parser():
     # Build the parser
-    parser = yacc.yacc()
-    path_parser = Path("./resources/parser_test.txt")
+    parser:LRParser = yacc()
+    path_parser = Path("./resources/parser_test_2.txt")
     code = path_parser.read_text()
     parser.parse(code)
